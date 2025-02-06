ee51c939193a986bf7618a0ffc0d88606d2ae52f
Airon <102803348+airon-aona@users.noreply.github.com>
2025-02-06 12:40:40 +0000

    #

--- js/gallery.js
+++ js/gallery.js
@@ -2,8 +2,13 @@ function OpenModal(src) {
     var modal = document.getElementById('modal');
     var img = document.getElementById('modalImg');
 
-    if(src !== 'undefined'){
         modal.classList.toggle('off');
         img.src = src
-    }
+
+        if(src !== undefined){
+            document.body.style.overflow = 'hidden';
+        }else{
+            document.body.style.overflow = 'auto';
+        }
+
 }
